6a32405baab644a4c80016399199c3deeee58182
zedpentium <zedpentium@gmail.com>
2021-06-09 13:09:52 +0200

    Exercise 1 klar.

--- LearningExercise PDF-file/Program.cs
+++ LearningExercise PDF-file/Program.cs
@@ -11,7 +11,19 @@ namespace LearningExercise_PDF_file
             {
                 try
                 {
-                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    Console.Clear();
+                    Console.WriteLine("----* Välkommen till Eric R's Learning Exercise PDF programmet *----\n");
+                    Console.WriteLine("Välj funktion:");
+                    Console.WriteLine();
+                    Console.WriteLine("1) Run Exercise One");
+                    Console.WriteLine("2) Run Exercise Two");
+                    Console.WriteLine("3) Tom");
+                    Console.WriteLine("4) Tom");
+                    Console.WriteLine("-1) Avsluta programmet");
+
+                    Console.Write("\r\nSkriv in NR på menyval & tryck enter: ");
+                    //Console.Write("Enter assignment number (or -1 to exit): ");
+
                     var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
                     Console.ForegroundColor = ConsoleColor.Green;
                     switch (assignmentChoice)
@@ -49,10 +61,12 @@ namespace LearningExercise_PDF_file
 
         private static void RunExerciseOne()
         {
-            string sFirstName;
-            string sLastName;
+            string sFirstName = "Eric";
+            string sLastName = "Rönnhult";
 
-            Console.WriteLine("You sucessfully ran excercise one!");
+            Console.WriteLine($"Hello {sFirstName} {sLastName}! I’m glad to inform you that you are the test subject of my very first assignment!");
+
+            //Console.WriteLine("You sucessfully ran excercise one!");
         }
 
         private static void RunExerciseTwo()
