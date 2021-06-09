64f803946a4c3d4b28b939d125646ef58b64a0c9
zedpentium <zedpentium@gmail.com>
2021-06-09 12:04:21 +0200

    Add project files.

---
+++ LearningExercise PDF-file/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LearningExercise_PDF_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case 3:
+                            // call your assignment method here!
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid assignment number!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static void RunExerciseOne()
+        {
+            string sFirstName;
+            string sLastName;
+            Console.WriteLine("You sucessfully ran excercise one!");
+        }
+
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You sucessfully ran excercise two!");
+        }
+
+    }
+}
