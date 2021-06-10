5810ab92ff5bb0b27fecf3db9ef5975cde8773e2
zedpentium <zedpentium@gmail.com>
2021-06-10 09:02:15 +0200

    Exercise 6 klar

--- LearningExercise PDF-file/Program.cs
+++ LearningExercise PDF-file/Program.cs
@@ -19,6 +19,16 @@ namespace LearningExercise_PDF_file
                     Console.WriteLine("3) Run Exercise 3");
                     Console.WriteLine("4) Run Exercise 4");
                     Console.WriteLine("5) Run Exercise 5");
+                    Console.WriteLine("6) Run Exercise 6");
+                    Console.WriteLine("7) Run Exercise 7");
+                    Console.WriteLine("8) Run Exercise 8");
+                    Console.WriteLine("9) Run Exercise 9");
+                    Console.WriteLine("10) Run Exercise 10");
+                    Console.WriteLine("11) Run Exercise 11");
+                    Console.WriteLine("12) Run Exercise 12");
+                    Console.WriteLine("13) Run Exercise 13");
+                    Console.WriteLine("14) Run Exercise 14");
+                    Console.WriteLine("15) Run Exercise 15");
                     Console.WriteLine("-1) Quit program");
 
                     Console.WriteLine("\nType in a number from the menu & press enter: ");
@@ -42,6 +52,36 @@ namespace LearningExercise_PDF_file
                         case 5:
                             RunExerciseFive();
                             break;
+                        case 6:
+                            RunExercise6();
+                            break;
+                        case 7:
+                            RunExercise7();
+                            break;
+                       /* case 8:
+                            RunExercise8();
+                            break;
+                        case 9:
+                            RunExercise9();
+                            break;
+                        case 10:
+                            RunExercise10();
+                            break;
+                        case 11:
+                            RunExercise11();
+                            break;
+                        case 12:
+                            RunExercise12();
+                            break;
+                        case 13:
+                            RunExercise13();
+                            break;
+                        case 14:
+                            RunExercise14();
+                            break;
+                        case 15:
+                            RunExercise15();
+                            break; */
                         case -1:
                             keepAlive = false;
                             Environment.Exit(0); // added to quicker finish program and nto have "Hit key to continue" before quit
@@ -93,8 +133,8 @@ namespace LearningExercise_PDF_file
         private static void RunExerciseThree()
         {
 
-            Console.Clear(); 
-            
+            Console.Clear();
+
             Console.Write("Enter your first name: ");
             string sFirstName = Console.ReadLine();
 
@@ -118,7 +158,7 @@ namespace LearningExercise_PDF_file
 
             // this is output info of what the string "str" contain
             Console.WriteLine($"Here is the str string variable:\n{str}\n");
-            
+
             // start of string manipulation
             strOutput = str.Replace("quick", "brown");
 
@@ -142,7 +182,7 @@ namespace LearningExercise_PDF_file
         private static void RunExerciseFive()
         {
             Console.Clear();
-            
+
             // Final output to screen must be [1,4,5,6,7,8,9,10]
 
             string str = "Arrays are very common in programming, they look something like: [1,2,3,4,5]";
@@ -165,5 +205,61 @@ namespace LearningExercise_PDF_file
             Console.WriteLine($"Output after string manipulation: {strOutput}");
 
         }
+
+        private static void RunExercise6()
+        {
+            // Decl of variables
+            int int1;
+            int int2;
+            int intBig;
+            int intSmall;
+            int intDif;
+            int intSum;
+            int intProd;
+            int intRat;
+            string str1;
+            string str2;
+
+            Console.Clear();
+            Console.Write("Input 2 integers, output biggest, smallers, difference -, sum +, product * and ratio /.\n\n");
+            Console.Write("Enter first integer: ");
+            str1 = Console.ReadLine();
+            Int32.TryParse(str1, out int1);
+
+            Console.Write("Enter second integer: ");
+            str2 = Console.ReadLine();
+            Int32.TryParse(str2, out int2);
+
+
+
+            Console.WriteLine($"\nOutput result of {int1} & {int2}");
+            if (int1 > int2)
+                {
+                intBig = int1;
+                intSmall = int2;
+                }
+            else
+                {
+                intBig = int2;
+                intSmall = int1;
+                }
+            Console.WriteLine($"\n\nBiggest: {intBig}");
+            Console.WriteLine($"Smallest: {intSmall}");
+
+            intDif = int1 - int2;
+            Console.WriteLine($"Difference (-): {intDif}");
+            intSum = int1 + int2;
+            Console.WriteLine($"Sum (+): {intSum}");
+            intProd = int1 * int2;
+            Console.WriteLine($"Product (*): {intProd}");
+            intRat = int1 / int2;
+            Console.WriteLine($"Ratio (/): {intRat}");
+        }
+
+        private static void RunExercise7()
+        {
+
+        }
     }
 }
+
